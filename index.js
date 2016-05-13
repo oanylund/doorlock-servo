6002b05d714f568b06db6a26cbdf2979b1154f49
oanylund <ole.a.nylund@gmail.com>
2016-05-13 02:10:14 +0000

    Added user authentication against db. Deleted express test server code

--- index.js
+++ index.js
@@ -1,57 +1,50 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-
 var doorlock = require('./doorlock.js');
 var MainLock = new doorlock();
 
 var alarmBuzzer = require('./alarm.js');
 var AlarmBuzzer = new alarmBuzzer();
-var rfid = require("rc522-rfid");
+var rfid = require('rc522-rfid');
 
-rfid(function(rfidSerialNumber){
-    console.log(rfidSerialNumber);
-    if( rfidSerialNumber === 'e8b513b9' ) {
-      console.log('User, Ony008!');
-      if( MainLock.IsLocked() ) {
-        MainLock.Unlock();
-        console.log('Open door!');
-        AlarmBuzzer.openSequence();
+var User = require('doorlock-models').User;
+
+User.sync().then(function() {
+  console.log('--------------------------------------------');
+  console.log('User schema synced.');
+  console.log(new Date());
+
+  rfid(function(rfidSerialNumber){
+
+    User.findOne({
+      where: { studentCardId: rfidSerialNumber },
+      attributes: ['firstName', 'lastName']
+    }).then(function(user) {
+      if( !user ) {
+        console.log('--------------------------------------------');
+        console.log('Unauthorized card: ' + rfidSerialNumber);
+        console.log(new Date());
+        // FEILPIPING fra buzzer
       }
       else {
-        MainLock.Lock();
-        console.log('Close door!');
-        AlarmBuzzer.closeSequence();
+        if( MainLock.IsLocked() ) {
+          MainLock.Unlock();
+          console.log('--------------------------------------------');
+          console.log('Door opened for user ' + user.firstName + ' ' + user.lastName);
+          console.log(new Date());
+          AlarmBuzzer.openSequence();
+        }
+        else {
+          MainLock.Lock();
+          console.log('--------------------------------------------');
+          console.log('Door closed for user ' + user.firstName + ' ' + user.lastName);
+          console.log(new Date());
+          AlarmBuzzer.closeSequence();
+        }
       }
+    });
 
-    }
+  });
+
 });
-
-// app.use(express.static(path.join(__dirname, 'public')));
-//
-// app.get('/', function(req,res) {
-//     res.sendFile(path.join(__dirname, 'index.html'));
-// });
-//
-// app.get('/open', function(req,res) {
-//   if( MainLock.IsLocked() ) {
-//     MainLock.Unlock();
-//     AlarmBuzzer.openSequence();
-//   }
-//   res.redirect('/');
-// });
-//
-// app.get('/close', function(req,res) {
-//   if( !MainLock.IsLocked() ) {
-//     MainLock.Lock();
-//     AlarmBuzzer.closeSequence();
-//   }
-//   res.redirect('/');
-// });
-//
-// app.listen(3000, function() {
-// 	console.log('Server started on port 3000');
-// });
 
 process.on('SIGINT', function () {
   console.log('\nService stopped by user. Bye');
