da2903ae5951999eefc270bf58c69929b731d560
oanylund <ole.a.nylund@gmail.com>
2016-06-14 19:57:01 +0200

    Added authenticated socket using jwt secret from api. Streams tail of log to UI

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+var spawn = require('child_process').spawn;
+var path = require('path');
+
 var Doorlock = require('./doorlock.js');
 var mainLock = new Doorlock();
 
@@ -28,8 +31,11 @@ logger('App started');
 
 // Socket.io stuff for getting new studentCardId to admin ui on registration
 var io = require('socket.io')(8080, { serveClient: false });
+var socketioJwt = require('socketio-jwt');
+var secret = require('../config.js').secret;
 
 var getId = io.of('/get-id');
+var auth = io.of('/auth');
 
 getId.on('connection', function (socket) {
   logger('New connection on socket 8080, namespace /get-id');
@@ -57,6 +63,27 @@ getId.on('connection', function (socket) {
     });
 
   });
+});
+
+auth.on('connection', socketioJwt.authorize({
+    secret: secret,
+    timeout: 15000
+}))
+.on('authenticated', function(socket) {
+  // Authenticated socket
+
+  // Create a child process
+  var logTail = spawn('tail',
+      ['-f', '-n', 12, path.resolve(__dirname, '../logs/doorlock-servo-out.log')]);
+
+  logTail.stdout.on('data', function (data) {
+    socket.emit('logTail', data.toString());
+  });
+
+  socket.on('disconnect', function() {
+    logTail.kill();
+  });
+
 });
 
 // Handle lock/unlock door
@@ -92,8 +119,11 @@ User.sync().then(function() {
     });
 });
 
-process.on('SIGINT', function () {
-  console.log('\nService stopped by user. Bye');
-  // Release GPIO pins used by onoff
-  mainLock.release();
+// Force process exit to only listen for exit event.
+var cleanExit = function() { process.exit() };
+process.on('SIGINT', cleanExit); // catch ctrl-c
+process.on('SIGTERM', cleanExit); // catch kill
+
+process.on('exit', function() {
+  // All cleanup necessary on exit goes here
 });
